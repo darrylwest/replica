774101f402505994eceb52d57830eae2ab842e5f
darryl west <dpw@raincitysoftware.com>
2022-01-26 12:04:35 -0800

    updated from cmake template

---
+++ src/main.h
@@ -0,0 +1,12 @@
+//
+// Created by Konstantin Gredeskoul on 10/7/18.
+//
+
+#ifndef PROJECT_MAIN_H
+#define PROJECT_MAIN_H
+
+static const char *const HEADER = "\nReplica © 2022 Rain City Software.\n\n";
+static const char *const USAGE = "Usage:\n\treplica ...";
+
+
+#endif //PROJECT_MAIN_H
---
+++ test/src/replica_tests.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Konstantin Gredeskoul on 5/16/17.
+//
+#include <replica.h>
+#include "gtest/gtest.h"
+
+using namespace std;
+
+
+
+class ReplicaTest : public ::testing::Test {
+
+protected:
+
+  virtual void SetUp() {
+  };
+
+  virtual void TearDown() {
+  };
+
+};
+
+
