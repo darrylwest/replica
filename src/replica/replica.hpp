79d773bd064d704ab3771b140bd06e6189435a96
darryl west <dpw@raincitysoftware.com>
2022-01-26 12:50:29 -0800

    initial partial build

--- src/replica/replica.hpp
+++ src/replica/replica.hpp
@@ -5,25 +5,31 @@
 #ifndef REPLICA_HPP
 #define REPLICA_HPP
 
+#include <filesystem>
+#include <chrono>
+#include <vector>
+#include <string>
+
 namespace replica {
-    namespace clk std::chrono::system_clock;
+    // namespace clk = std::chrono::system_clock;
     namespace fs = std::filesystem;
 
     struct PollSpec {
         bool enabled;
-        std::chrono::duration interval;
+        // std::chrono::duration interval;
+        long interval;
     };
 
     struct BackupSpec {
         std::string version;
         std::string name;
-        vector<std::string> src_folders;
-        vector<std::string> targets;
+        std::vector<std::string> src_folders;
+        std::vector<std::string> targets;
         bool compress;
-        vector<std::string> compression_rules;
+        std::vector<std::string> compression_rules;
         bool encrypt;
-        vector<std::string> encryption_rules;
-        PollSpec poll_spec;
+        std::vector<std::string> encryption_rules;
+        replica::PollSpec poll_spec;
     };
 
     struct FileSpec {
@@ -31,8 +37,8 @@ namespace replica {
         std::string sha;
         std::uintmax_t size;
         fs::file_time_type last_modified;
-        clk::time_point last_replica;
-        FileSpec last_file_spec;
+        std::chrono::system_clock::time_point last_replica;
+        // replica::FileSpec last_file_spec;
     };
 }
 
