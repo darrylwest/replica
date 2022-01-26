ba25f5d4bf3a26443d776c60a17e05814c0a8d94
darryl west <dpw@raincitysoftware.com>
2022-01-26 13:59:53 -0800

    basic implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <division.h>
+#include <replica.hpp>
 #include <iostream>
 
 using namespace std;
@@ -7,8 +7,6 @@ static const char *const HEADER = "\nReplica © 2022 Rain City Software.\n\n";
 static const char *const USAGE = "Usage:\n\treplica ...";
 
 int main(int argc, const char *argv[]) {
-  Fraction f;
-
   cout << HEADER;
 
   // ensure the correct number of parameters are used.
