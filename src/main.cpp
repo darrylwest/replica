312c9724e9b48647184347e183a588d7eeee8aef
darryl west <dpw@raincitysoftware.com>
2022-01-27 13:43:51 -0800

    refactored for clion

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,8 @@
 
 #include <iostream>
-#include "replica.hpp"
+#include "../include/replica.hpp"
 
-int main(int argc, char *argv[]) {
-    replica::Config config = replica::parse(argc, argv);
-
-    std::cout << "dry run? " << config.dryrun << std::endl;
-    std::cout << "replica version: " << replica::APP_VERSION << std::endl;
-
+int main() {
+    std::cout << "Hello, Replica, Version: " << replica::APP_VERSION << std::endl;
     return 0;
 }
-
