3bd7f30acdf3644a81138858d386aa252d352b66
darryl west <dpw@raincitysoftware.com>
2022-01-30 13:35:29 -0800

    version bump

--- include/replica.hpp
+++ include/replica.hpp
@@ -11,7 +11,7 @@
 #include "cxxopts.hpp"
 
 namespace replica {
-    const char* APP_VERSION = "22.1.29";
+    const char* APP_VERSION = "22.1.30";
     const char* BANNER = "Replica File Exchange Service © 2022 Rain City Software";
 
     struct PollSpec {
@@ -83,6 +83,7 @@ namespace replica {
             }
         } catch (const cxxopts::OptionException& e) {
             std::cout << "error parsing options: " << e.what() << std::endl;
+            config.skip = true;
         }
 
         config.poll_spec = poll_spec;
