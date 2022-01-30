5bcc2aced686b5a2d220f45464171eb24cc20e0e
darryl west <dpw@raincitysoftware.com>
2022-01-30 09:18:00 -0800

    test updates

--- test/main.cpp
+++ test/main.cpp
@@ -6,8 +6,25 @@
 #include "../include/replica.hpp"
 
 TEST_CASE("construct", "[create]") {
-  SECTION("test version") {
-    auto const vers = replica::APP_VERSION;
-    REQUIRE(vers != nullptr);
-  }
+    SECTION("test version") {
+        auto const vers = replica::APP_VERSION;
+        REQUIRE(vers != nullptr);
+    }
+    SECTION("test banner") {
+        auto const banner = replica::APP_VERSION;
+        REQUIRE(banner != nullptr);
+    }
+
+    SECTION("poll spec struct - default") {
+        auto poll_spec = replica::PollSpec();
+        REQUIRE(poll_spec.enabled == false);
+        REQUIRE(poll_spec.interval == 60000);
+    }
+
+    SECTION("poll spec struct - custom") {
+        auto interval = 5000;
+        auto poll_spec = replica::PollSpec{true, interval};
+        REQUIRE(poll_spec.enabled == true);
+        REQUIRE(poll_spec.interval == interval);
+    }
 }
