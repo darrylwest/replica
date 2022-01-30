59bdf3aaea581ff19a974ef6ad77cc76aeb24d37
darryl west <darryl.west@RainCitySoftware.com>
2022-01-30 14:41:31 -0800

    added vscode and moved banner

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,15 @@
 #include "replica.hpp"
 
 int main(int argc, char *argv[]) {
-    std::cout << replica::BANNER << '\n';
-    std::cout << "Version: " << replica::APP_VERSION << std::endl;
-
     replica::Config config = replica::parse(argc, argv);
 
     // TODO : move this part to process module to enable unit tests...
     if (config.skip) {
         return 0;
     } else if (config.dryrun) {
+        std::cout << replica::BANNER << '\n';
+        std::cout << "Version: " << replica::APP_VERSION << std::endl;
+
         // show the complete config...
         std::cout << "Config: name: " << config.name << std::endl;
         std::cout << "Config: home: " << config.replica_home << std::endl;
